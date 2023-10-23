357a3ee07e5c242fbbb85aab0cf2bf7ffb21502e
THETOUCH <super.ilgiznex@gmail.com>
2023-10-23 16:40:18 +0600

    123

--- src/main/java/com/epam/mjc/stage0/Bird.java
+++ src/main/java/com/epam/mjc/stage0/Bird.java
@@ -1,5 +1,22 @@
 package com.epam.mjc.stage0;
 
-public class Bird {
-
+public class Bird extends Animal {
+    public Bird() {
+        super("blue", 2, false);
+    }
+    @Override
+    public String getDescription() {
+        String s = super.getDescription();
+        String z = " Moreover, it has 2 wings and can fly.";
+        return s+z;
+    }
+    public static void main(String[] args) {
+        Bird bird = new Bird();
+        bird.getDescription();
+        Dog dog = new Dog();
+        dog.getDescription();
+        Animal animal = new Animal("red", 3, false);
+        animal.getDescription();
+    }
 }
+
